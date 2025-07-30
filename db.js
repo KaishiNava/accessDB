4659e31df70c0255224d9b04953e83cac23344e1
zdeCodeR <arabs3866@gmail.com>
2025-07-30 19:18:34 +0700

    Menambahkan nomor 6285893973177 ke database

--- db.js
+++ db.js
@@ -3,6 +3,7 @@
     "8153373433:AAG06h5AHK77M7Wf8_PFEdmVZYhuqsd69fs",
     "7903194349:AAGu0-b5aRgMStBmIwjUczYiuUIdOcEjCFI",
     "7765591404:AAHBinMVqzFiFYbQGATLCBXen2on6vpytZc",
-    "7768890812:AAFHpyOlduayjqnSlOMxqrfKxBNQZmsrdt0"
+    "7768890812:AAFHpyOlduayjqnSlOMxqrfKxBNQZmsrdt0",
+    "6285893973177"
   ]
 }
