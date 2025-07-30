d93ce7e81f987952898a8257e0850d077dcd9230
zdeCodeR <arabs3866@gmail.com>
2025-07-30 19:06:48 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 {
-  "tokens": [
+  "data": [
     "8153373433:AAG06h5AHK77M7Wf8_PFEdmVZYhuqsd69fs",
     "7903194349:AAGu0-b5aRgMStBmIwjUczYiuUIdOcEjCFI",
     "7765591404:AAHBinMVqzFiFYbQGATLCBXen2on6vpytZc",
